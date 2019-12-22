5eb048caed0f68dcad9566b43f9559eb12d8acbb
ccccye <w0.0.3@163.com>
2019-12-23 00:10:40 +0800

    完成定位、穿衣、湿度、PM2.5功能

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,114 +1,128 @@
 //index.js
 const app = getApp()
 
+// 引入腾讯地图SDK核心类
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
+
 Page({
   data: {
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: '',
   },
   
   onLoad: function() {
-    
-    var obj = this
+    this.qqmapsdk = new QQMapWX({
+      key: 'ESYBZ-26E36-MOXSA-MDBNS-XVEIF-PDBIW'
+    })  
 
+    this.getCityAndWeather()
+  },
+
+  // 获取位置及天气
+  getCityAndWeather() {
+    var that = this;
+    wx.getLocation({
+      success: res => {
+        this.location_pin = res.longitude + ',' + res.latitude
+        this.qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: res2 => {
+            let city = res2.result.address_component.city
+            that.setData({
+              location_text: city,
+            })
+
+            var nation_code = res2.result.ad_info.nation_code
+            var city_code = res2.result.ad_info.city_code
+            var nation_index = city_code.indexOf(nation_code)
+            if (nation_index == 0) {
+              city_code = city_code.substring(nation_code.length)
+            
+              that.getNowWeather(city_code)
+            } else {
+              // 没有省级代码
+            }
+
+          }
+        })
+      },
+      fail: () => {
+        console.log('未授权位置');
+      }
+    })
+  },
+
+  // 获取当前天气
+  getNowWeather(adCode) {
+    var that = this
+    console.log(adCode)
+
+    // 获取实时预告
     wx.request({
       url: 'https://www.ccccye.cn/weather/today',
+      data: { adcode: adCode},
       complete(res) {
+        console.log(res)
         if (res.statusCode == 200) {
-          obj.setData({
-            weather: res.data
+          that.setData({
+            city: res.data.city,
+            temp: res.data.temp,
+            weather: res.data.weather,
+            today: res.data.today,
+            wd: res.data.wd,
+            humidity: res.data.humidity,
+            pm25: res.data.pm25
           })
         }
         else {
-          console.log(res)
+
         }
       }
     })
 
-  },
+    //获取生活指数
+    wx.request({
+      url: 'https://www.ccccye.cn/weather/life',
+      data: { adcode: adCode},
+      complete(res) {
+        console.log(res)
+        if (res.statusCode == 200) {
+          that.setData({
+            air: res.data.data.zs_kqwr.type,
+            proposeCY: res.data.data.zs_cy.info
+          })
+        }
+        else {
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
+        }
       }
     })
-  },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
 
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
+    // 获取未来6天的天气预告
+    wx.request({
+      url: 'https://www.ccccye.cn/weather/d6',
+      data: { adcode: adCode },
+      complete(res) {
+        console.log(res)
+        if (res.statusCode == 200) {
+          // that.setData({
             
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
+          // })
+        }
+        else {
 
-      },
-      fail: e => {
-        console.error(e)
+        }
       }
     })
-  },
+
+  }
 
 })
